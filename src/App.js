858e02af2347d22446aff311508e7c931d071229
Lindsey Bell <lindseywb@gmail.com>
2022-01-04 11:37:07 -0500

    feautre/cards add the card values functionality and the deal cards button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Score from './components/Score';
 import Deck from './components/Deck';
 
 function App() {
+
+  const [cardValues, setCardValues] = useState(newCardValues());
+
+  function newCardValues() {
+    const newValues = {
+      card1: Math.ceil( Math.random() * 12 ),
+      card2: Math.ceil( Math.random() * 12 )
+    }
+
+    return newValues;
+  }
+
+  function dealCards() {
+    setCardValues(newCardValues())
+  }
+
   return (
     <div className="App">
       <Header />
-      <Deck />
+      <Deck values={cardValues} dealCards={dealCards} />
       <Score />
     </div>
   );
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -2,11 +2,13 @@ import CardPlayer1 from "./CardPlayer1";
 import CardPlayer2 from "./CardPlayer2";
 
 export default function Deck(params) {
+
     return (
         <div className="deck">
-            cards will be dealt here.
-            <CardPlayer1 />
-            <CardPlayer2 />
+            <CardPlayer1 value={params.values.card1} />
+            <CardPlayer2 value={params.values.card2} />
+
+            <button onClick={params.dealCards}>Deal</button>
         </div>
     )
 };
