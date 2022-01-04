a4e54fb392fc2172cecbe3ec0896ffb5d4a5e001
Lindsey Bell <lindseywb@gmail.com>
2022-01-04 10:45:14 -0500

    feature/game-skeleton change card component to be 2 separate cards

--- src/components/CardPlayer2.js
+++ src/components/CardPlayer2.js
@@ -1,4 +1,4 @@
-export default function Card(params) {
+export default function CardPlayer2(params) {
     return (
         <div className="card">
             This is a card.
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,10 +1,12 @@
-import Card from "./Card";
+import CardPlayer1 from "./CardPlayer1";
+import CardPlayer2 from "./CardPlayer2";
 
 export default function Deck(params) {
     return (
         <div className="deck">
             cards will be dealt here.
-            <Card />
+            <CardPlayer1 />
+            <CardPlayer2 />
         </div>
     )
 };
