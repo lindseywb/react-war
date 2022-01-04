1ee924a3399090134cebfe993b3860cb7c9d29fa
Lindsey Bell <lindseywb@gmail.com>
2022-01-04 12:57:51 -0500

    feature/cards add startWar boolean and basic logic for war

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Score from './components/Score';
@@ -6,16 +6,25 @@ import Deck from './components/Deck';
 
 function App() {
 
+  const [startWar, setStartWar] = useState(false);
   const [cardValues, setCardValues] = useState(newCardValues());
 
   function newCardValues() {
+    
     const newValues = {
       card1: Math.ceil( Math.random() * 12 ),
       card2: Math.ceil( Math.random() * 12 )
     }
-
     return newValues;
   }
+
+  useEffect(() => {
+    if(cardValues.card1 === cardValues.card2) {
+      setStartWar(true)
+    } else {
+      setStartWar(false)
+    }
+  }, [cardValues])
 
   function dealCards() {
     setCardValues(newCardValues())
@@ -24,7 +33,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Deck values={cardValues} dealCards={dealCards} />
+      <Deck values={cardValues} dealCards={dealCards} war={startWar} />
       <Score />
     </div>
   );
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -2,11 +2,13 @@ import CardPlayer1 from "./CardPlayer1";
 import CardPlayer2 from "./CardPlayer2";
 
 export default function Deck(params) {
-
+    // console.log(params.values)
     return (
         <div className="deck">
             <CardPlayer1 value={params.values.card1} />
             <CardPlayer2 value={params.values.card2} />
+
+            {params.war && 'its war time'}
 
             <button onClick={params.dealCards}>Deal</button>
         </div>
